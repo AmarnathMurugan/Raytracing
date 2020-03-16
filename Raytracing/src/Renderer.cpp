7f9b3557ab2633ff85061880fd8ad20bc86206f0
Amarnth <amarnath2105@gmail.com>
2020-03-17 01:34:25 +0530

    Rendering done with abstracted hitable array

--- Raytracing/src/Renderer.cpp
+++ Raytracing/src/Renderer.cpp
@@ -1,20 +1,27 @@
-//#include<iostream>
-//#include "Headers/Vector3.h"
-#include "Headers/Ray.h"
 
-Vector3 ColorAtRay(const Ray& ray);
-double isPointAtSphere(Vector3 center, double Radius, const Ray& ray);
+#include "Headers/Hitable_list.h"
+#include "Headers/Sphere.h"
+
+Vector3 ColorAtRay(const Ray& ray, HitableList& world ,HitRecord& hitRecord);
+ 
 
 int main()
 {
 	std::cout << "P3\n";
 	int Width = 200;
 	int Height = 100;
+	std::cout << Width << " " << Height << "\n255\n";
+
 	Vector3 LowerLeftCorner(-2, -1, -1);
 	Vector3 Horizontal(4, 0, 0);
 	Vector3 Vertical(0, 2, 0);
 	Vector3 Origin(0, 0, 0);
-	std::cout << Width << " " << Height << "\n255\n";
+
+	Hitable* ModelArrays[2];
+	ModelArrays[0] = new Sphere(Vector3(0, 0, -1), 0.5);
+	ModelArrays[1] = new Sphere(Vector3(0, -100.5, -1), 100);
+	HitableList World(ModelArrays, 2);
+	HitRecord hitRecord;
 	for (int y = Height - 1; y >= 0; y--)
 	{
 		for (int x = 0; x < Width; x++)
@@ -22,7 +29,7 @@ int main()
 			float U = x / (float)Width;
 			float V = y / (float)Height;
 			Ray ray(Origin, LowerLeftCorner + U*Horizontal + V * Vertical);
-			Vector3 color = ColorAtRay(ray);
+			Vector3 color = ColorAtRay(ray,World, hitRecord);
 			int r = (int)(255.99 * color.r());
 			int g = (int)(255.99 * color.g());
 			int b = (int)(255.99 * color.b());
@@ -36,33 +43,18 @@ int main()
 	std::cin.get();
 }
 
-Vector3 ColorAtRay(const Ray& ray)
+Vector3 ColorAtRay(const Ray& ray, HitableList& world  ,HitRecord& hitRecord)
 {
-	Vector3 SphereCenter(0, 0, -1);
-	double SphereRadius = 0.5;
-	double t = isPointAtSphere(SphereCenter, SphereRadius, ray);
-	if (t >= 0)
+
+	if (world.isHit(ray, 0, 100, hitRecord))
 	{
-		Vector3 Normal = (ray.Point_On_Ray(t) - SphereCenter).normalized();
-		return 0.5 * Vector3(Normal.r() + 1, Normal.g() + 1, Normal.b() + 1);
+		return 0.5 * Vector3(hitRecord.Normal.r()+1, hitRecord.Normal.g() + 1, hitRecord.Normal.b() + 1);
 	}
+	
 	Vector3 NormalizedDirection = ray.Ray_Direction().normalized();
 	double NormalizedY = 0.5*(NormalizedDirection.y() + 1);
 	return (1 - NormalizedY)*Vector3(1.0, 1.0, 1.0) + NormalizedY * Vector3(.5, .7, 1.0);
 
 }
 
-double isPointAtSphere(Vector3 center, double Radius, const Ray& ray)
-{
-	Vector3 DirectionFromCenterToStart = ray.Start_Point() - center;
-	double A = DotProduct(ray.Ray_Direction(), ray.Ray_Direction());
-	double B = 2 * DotProduct(ray.Ray_Direction(), DirectionFromCenterToStart);
-	double C = DotProduct(DirectionFromCenterToStart, DirectionFromCenterToStart) - (Radius * Radius);
-	double Discriminant = (B*B) - 4 * A*C;
-	if (Discriminant > 0)
-		return (-B - sqrt(Discriminant)) / (2.0 * A);
-	else
-		return -1.0; // negative t implies that the object is behind camera
-	 
-}
 
