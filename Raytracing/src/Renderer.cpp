b97ef7092805d2bfa1ff92ed774d97aa3a9d768c
Amarnath <amarnath2105@gmail.com>
2020-04-02 00:51:20 +0530

    Init update to v3
    - Image data made const
    - project settings save to ppm through redirection in command line

--- Raytracing/src/Renderer.cpp
+++ Raytracing/src/Renderer.cpp
@@ -13,27 +13,29 @@ unsigned long int count = 0;
 
 int main()
 {
-	srand((unsigned)time(0));
-	int Width = 1920;
-	int Height = 1080;
-	int Samples = 100;
-	std::fstream OutputFile("Output.txt", std::ios::out | std::ios::trunc);
-	OutputFile << "P3\n";
-	OutputFile << Width << " " << Height << "\n255\n";
-	Vector3 LookFrom(13, 2, 3);
-	Vector3 LookAt(0, 0, 0);
-	Vector3 ViewUp(0, 1, 0);
-	double focalDistance = (LookFrom - LookAt).length();
-	double aperture = 0.1;
-	Camera cam(30,(Width/(double)Height),aperture,focalDistance,LookFrom,LookAt,ViewUp);
-	HitableList* World = GetRandomWorld(); 
-	for (int y = Height - 1; y >= 0; y--)
+	//srand((unsigned)time(0));
+	const int Width = 200;
+	const int Height = 100;
+	const int Samples = 100;
+	std::cout << "P3\n";
+	std::cout << Width << " " << Height << "\n255\n";
+	//Vector3 LookFrom(13, 2, 3);
+	//Vector3 LookAt(0, 0, 0);
+	//Vector3 ViewUp(0, 1, 0);
+	//double focalDistance = (LookFrom - LookAt).length();
+	//double aperture = 0.1;
+	//Camera cam(30,(Width/(double)Height),aperture,focalDistance,LookFrom,LookAt,ViewUp);
+	//HitableList* World = GetRandomWorld(); 
+	
+	for (int y = Height - 1; y >= 0; --y)
 	{
-		for (int x = 0; x < Width; x++)
+		for (int x = 0; x < Width; ++x)
 		{			
-			std::cout << "Percent Complete : " << ((Width)*(Height-y-1)+(x+1))*100/(float)(Width*Height) << "  x : " << x << "  y : " << y ;
-			Vector3 color(0,0,0);
-			for (int s = 0; s < Samples; s++)
+			//std::cout << "Percent Complete : " << ((Width)*(Height-y-1)+(x+1))*100/(float)(Width*Height) << "  x : " << x << "  y : " << y ;
+			std::cerr << "\rScanlines Remaining: " << y << ' ' << std::flush;
+			//Vector3 color(0,0,0);
+			Vector3 color( x/(double)Width , y/(double)Height, .6);
+		/*	for (int s = 0; s < Samples; s++)
 			{
 				float U = (x + rand01()) / (float)Width;
 				float V = (y + rand01()) / (float)Height;
@@ -41,18 +43,16 @@ int main()
 				color += ColorAtRay(ray, World, 0);
 			}
 			color /= (double)Samples;
-			color = Vector3(sqrt(color.r()), sqrt(color.g()), sqrt(color.b()));
-			int r = (int)(255.99 * color.r());
-			int g = (int)(255.99 * color.g());
-			int b = (int)(255.99 * color.b());
-			std::cout << "\r";
-			//std::cout << r << " " << g << " " << b<<"  ";			 
-			OutputFile << r << " " << g << " " << b << "  ";
+			color = Vector3(sqrt(color.r()), sqrt(color.g()), sqrt(color.b()));*/
+			int r = static_cast<int>(255.99 * color.r());
+			int g = static_cast<int>(255.99 * color.g());
+			int b = static_cast<int>(255.99 * color.b());
+			std::cout << r << " " << g << " " << b<<"  ";				 
 		}
-		//std::cout << std::endl;
-		OutputFile << std::endl;
+		std::cout << std::endl;		 
 	}
-	OutputFile.close();
+
+	std::cerr << "\n DONE";
 	std::cin.get();
 }
 
@@ -78,10 +78,10 @@ Vector3 ColorAtRay(const Ray& ray, HitableList* world, int depth)
 
 HitableList * GetRandomWorld()
 {
-	Hitable** ModelArrays= new Hitable*[500];
+	Hitable** ModelArrays= new Hitable*[4];
 	ModelArrays[0] = new Sphere(Vector3(0, -1000.5, -1), 1000, new Lambertian(Vector3(0.5, 0.5, 0.5)));
 	int i = 1;
-	for (int a = -11; a < 11; a++)
+	/*for (int a = -11; a < 11; a++)
 	{
 		for (int b = -11; b < 11; b++)
 		{
@@ -98,8 +98,8 @@ HitableList * GetRandomWorld()
 			}
 
 		}
-	}
-	ModelArrays[i++] = new Sphere(Vector3(0,1,0), 1, new Dielectric(1.5));
+	}*/
+	ModelArrays[i++] = new Sphere(Vector3(0,1,0), 1, new Metal(Vector3(0.8, 0.8, 0.8), 0.0));
 	ModelArrays[i++] = new Sphere(Vector3(-4,1,0), 1, new Lambertian(Vector3(0.3, 0.5, 0.1)));
 	ModelArrays[i++] = new Sphere(Vector3(4,1,0),1, new Metal(Vector3(0.7, 0.6, 0.5), 0.0));
 	return new HitableList(ModelArrays, i);
