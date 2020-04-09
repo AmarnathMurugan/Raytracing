c9aa9943559983ad551679cd5f84332e88454f37
Amarnath <amarnath2105@gmail.com>
2020-04-09 06:13:19 +0530

    Multithreading Implemented
    Render time reduced from 135 seconds to 35 seconds (3.85x decrease) for a 200 x 100 image with sampling at 200

--- Raytracing/src/Renderer.cpp
+++ Raytracing/src/Renderer.cpp
@@ -3,66 +3,81 @@
 #include "Headers/Camera.h"
 #include "Headers/Material.h"
 #include <thread>
+#include <mutex>
 #include <chrono>
-Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth);
 
-Vector3 RandomPointInUnitSphere();
-unsigned long int count = 0;
+#pragma region FUNCTION_SIGNATURES
+	HitableList GetWorld();
+	Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth);
+	void GetChunkRange(int& Start, int& End);
+	void RenderImage(int ThreadIndex, HitableList& World, Camera& cam);
 
-//> "$(ProjectDir)Renders\Output.ppm"
-
-int main()
-{ 
-	const int Width = 200;
-	const int Height = 100;
+#pragma endregion
+	   
+#pragma region PUBLIC_VARIABLES
+	const int imageWidth = 200;
+	const int imageHeight = 100;
 	const int Samples = 200;
 	const int MaxDepth = 100;
 
+	const int NumberOfThreads = std::thread::hardware_concurrency();
+	const int ChunkSize = imageHeight / NumberOfThreads;
+
+	int NextHeight = imageHeight - 1;
+	Vector3 finalBuffer[imageHeight][imageWidth];
+	std::mutex ChunkRangeMutex;
+	bool isComplete = false;
+#pragma endregion
+	   
+int main()
+{ 	
 	std::cout << "P3\n";
-	std::cout << Width << " " << Height << "\n255\n"; 
+	std::cout << imageWidth << " " << imageHeight << "\n255\n"; 
 
 	Vector3 LookFrom(-2, 2, 1);
 	Vector3 LookAt(0, 0, -1);
 	Vector3 ViewUp(0, 1, 0);
 	double focalDistance = (LookFrom - LookAt).length();
 	double aperture = 0.01;
-	Camera cam(30,(Width/(double)Height),aperture,focalDistance,LookFrom,LookAt,ViewUp);
+	Camera cam(30,(imageWidth/(double)imageHeight),aperture,focalDistance,LookFrom,LookAt,ViewUp);
 	
-	HitableList World(make_shared<Sphere>( Vector3(0, 0, -1) , 0.5, make_shared<Lambertian>(Vector3(0.7,0.3,0.3))));
-	World.Add(make_shared<Sphere>( Vector3(0, -100.5, -1) , 100, make_shared<Lambertian>(Vector3(0.8,0.8,0))));
-	World.Add(make_shared<Sphere>(Vector3(1, 0, -1), 0.5, make_shared<Metal>(Vector3(0.8, 0.6, 0.2), 0.0)));
-	World.Add(make_shared<Sphere>(Vector3(-1, 0, -1), 0.5, make_shared<Dielectric>(1.5)));
-	Vector3 color(0, 0, 0);	
+	HitableList World = GetWorld();
+
 	auto StartTime = std::chrono::steady_clock::now();
 
-	for (int y = Height - 1; y >= 0; --y)
+	std::vector<std::thread> threads;
+	threads.reserve(NumberOfThreads);
+	for (int i = 0; i < NumberOfThreads; i++)
+		threads.emplace_back(std::thread(RenderImage, i, std::ref(World), std::ref(cam)));	
+	for (std::thread &t : threads)
+		if (t.joinable()) t.join();
+	
+
+	for (int y = imageHeight - 1; y >= 0; --y)
 	{
-		for (int x = 0; x < Width; ++x)
-		{			
-			std::cerr << "\rPercent Complete : " << ((Width)*(Height-y-1)+(x+1))*100/(float)(Width*Height) << "  x : " << x << "  y : " << y << ' ' << std::flush;
-			 
-			for (int s = 0; s < Samples; s++)
-			{
-				double U = (x + RandomDouble()) / (double)Width;
-				double V = (y + RandomDouble()) / (double)Height;
-				Ray ray = cam.GetRayAtUV(U, V);
-				Vector3 SamplingColor= ColorAtRay(ray, World, MaxDepth);
-				double red = SamplingColor.r();
-				color += SamplingColor;
-			}
-			color /= (double)Samples;
-			color = Vector3(sqrt(color.r()), sqrt(color.g()), sqrt(color.b()));			 
-			int r = static_cast<int>(256 *  Clamp(color.r(),0,0.999));
-			int g = static_cast<int>(256 *  Clamp(color.g(),0,0.999));
-			int b = static_cast<int>(256 *  Clamp(color.b(),0,0.999));		 
-			std::cout << r << " " << g << " " << b<< " ";				 
+		for (int x = 0; x < imageWidth; ++x)
+		{
+			int r = static_cast<int>(256 * Clamp(finalBuffer[y][x].r(), 0, 0.999));
+			int g = static_cast<int>(256 * Clamp(finalBuffer[y][x].g(), 0, 0.999));
+			int b = static_cast<int>(256 * Clamp(finalBuffer[y][x].b(), 0, 0.999));
+			std::cout << r << " " << g << " " << b << " ";
 		}
-		std::cout << "\n";		 
+		std::cout << "\n";
 	}
+ 
 	auto EndTime = std::chrono::steady_clock::now();
-	std::cerr << "\n\aCompleted in : "<<std::chrono::duration_cast<std::chrono::minutes>(EndTime-StartTime).count();
-	 
+	std::cerr << "\n\aCompleted in : " << std::chrono::duration_cast<std::chrono::seconds>(EndTime - StartTime).count();
+
 	std::cin.get();
+}
+
+HitableList GetWorld()
+{
+	HitableList World(make_shared<Sphere>(Vector3(0, 0, -1), 0.5, make_shared<Lambertian>(Vector3(0.7, 0.3, 0.3))));
+	World.Add(make_shared<Sphere>(Vector3(0, -100.5, -1), 100, make_shared<Lambertian>(Vector3(0.8, 0.8, 0))));
+	World.Add(make_shared<Sphere>(Vector3(1, 0, -1), 0.5, make_shared<Metal>(Vector3(0.8, 0.6, 0.2), 0.0)));
+	World.Add(make_shared<Sphere>(Vector3(-1, 0, -1), 0.5, make_shared<Dielectric>(1.5)));
+	return World;
 }
 
 Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth)
@@ -87,7 +102,43 @@ Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth)
 	}
 }
 
+void GetChunkRange(int& StartValue, int& EndValue)
+{
+	StartValue = NextHeight;
+	EndValue = (int)ffmax((NextHeight - ChunkSize + 1), 0);
+	NextHeight -= ChunkSize;
+	if (NextHeight < 0)
+		isComplete = true;
+}
 
+void RenderImage(int ThreadIndex, HitableList& World, Camera& cam)
+{
+	int StartValue, EndValue;
+	Vector3 color;
 
+	ChunkRangeMutex.lock();
+	GetChunkRange(StartValue, EndValue);
+	ChunkRangeMutex.unlock();
+
+	for (int y = StartValue; y >= EndValue; --y)
+	{
+		for (int x = 0; x < imageWidth; ++x)
+		{		
+
+			for (int s = 0; s < Samples; s++)
+			{
+				double U = (x + RandomDouble()) / (double)imageWidth;
+				double V = (y + RandomDouble()) / (double)imageHeight;
+				Ray ray = cam.GetRayAtUV(U, V);
+				Vector3 SamplingColor = ColorAtRay(ray, World, MaxDepth);
+				double red = SamplingColor.r();
+				color += SamplingColor;
+			}
+			color /= (double)Samples;
+			finalBuffer[y][x] = Vector3(sqrt(color.r()), sqrt(color.g()), sqrt(color.b()));	
+		}
+	}
+ 
+}
 
 
