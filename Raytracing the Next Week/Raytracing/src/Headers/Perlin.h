89958384d51da14e98b94be36512fc7e76ed2bc1
Amarnath <amarnath2105@gmail.com>
2020-05-18 19:44:19 +0530

    Perlin noise and turbulent noise implemented

--- Raytracing the Next Week/Raytracing/src/Headers/Perlin.h
+++ Raytracing the Next Week/Raytracing/src/Headers/Perlin.h
@@ -9,10 +9,11 @@
 		Perlin();
 		~Perlin();
 		double PerlinNoise(const Vector3& p) const;
+		double turbulentNoise(const Vector3 &p, double layers) const;
 	private:
-		static const int pointCount = 256;
-		static const int mask = pointCount-1;
-		double *randvalues;
+		static const int RandCount = 256;
+		static const int mask = RandCount-1;
+		Vector3 *randVectors;
 		int *perm_x,*perm_y,*perm_z;
 
 		int* GetPermValues();
@@ -20,10 +21,10 @@
 
 	Perlin::Perlin()
 	{
-		randvalues = new double[pointCount];
-		for (int i = 0; i < pointCount; i++)
+		randVectors = new Vector3[RandCount];
+		for (int i = 0; i < RandCount; i++)
 		{
-			randvalues[i] = RandomDouble();
+			randVectors[i] = RandomPointOnUnitSphere();
 		}
 		perm_x = GetPermValues();
 		perm_y = GetPermValues();
@@ -32,7 +33,7 @@
 
 	Perlin::~Perlin()
 	{
-		delete[] randvalues;
+		delete[] randVectors;
 		delete[] perm_x;
 		delete[] perm_y;
 		delete[] perm_z;
@@ -40,16 +41,62 @@
 
 	double Perlin::PerlinNoise(const Vector3 &p) const
 	{
-		int x = ((int)(10*p.x())) & mask;
-		int y = ((int)(10*p.y())) & mask;
-		int z = ((int)(10*p.z())) & mask;
-		return randvalues[perm_x[x]^perm_y[y]^perm_z[z]];
+		double scale = 5;
+
+		int x = (int)(floor(p.x()));
+		int y = (int)(floor(p.y()));
+		int z = (int)(floor(p.z()));
+		double u = (p.x() - floor(p.x()));
+		double v = (p.y() - floor(p.y()));
+		double w = (p.z() - floor(p.z()));
+		double t1 = SmoothStep(u);
+		double t2 = SmoothStep(v);
+		double t3 = SmoothStep(w);
+
+		Vector3 cornerValues[2][2][2];
+		double dotPrdtValues[2][2][2];
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				for (int k = 0; k < 2; k++)
+				{
+					int index = perm_x[(x + i) & mask] ^ perm_y[(y + j) & mask] ^ perm_z[(z + k) & mask];
+					auto temp = randVectors[index];
+					cornerValues[i][j][k] = temp;
+					dotPrdtValues[i][j][k] = DotProduct(cornerValues[i][j][k], Vector3(i-u, j-v, k-w));
+				}
+
+		double accumulatedValue = 0;
+
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 2; j++)
+				for (int k = 0; k < 2; k++)
+				{
+					accumulatedValue += (t1 * i + (1-t1) * (1 - i)) * 
+										(t2 * j + (1-t2) * (1 - j)) * 
+										(t3 * k + (1-t3) * (1 - k)) * dotPrdtValues[i][j][k];
+				}	
+		return accumulatedValue;
+	}
+
+	double Perlin::turbulentNoise(const Vector3 &p, double layers) const
+	{
+		double accumulatedValue = 0;
+		double frequency = 1;
+		double scale = 1;
+		for (size_t i = 0; i < layers; i++)
+		{
+			accumulatedValue += PerlinNoise(p * frequency) * scale;
+			scale /= 2;
+			frequency *= 2;
+		}
+		return fabs(accumulatedValue);
 	}
 
 	int* Perlin::GetPermValues()
 	{
-		int *arr = new int[pointCount];
-		for (int i = 0; i < pointCount; i++)
+		int *arr = new int[RandCount];
+		for (int i = 0; i < RandCount; i++)
 		{
 			int temp = RandomInt(0,i);
 			if (temp != i)
