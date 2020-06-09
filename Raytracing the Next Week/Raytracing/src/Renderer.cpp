e5a127d9fcd10d5b645b05e4c157ad2e522006d5
Amarnath <amarnath2105@gmail.com>
2020-06-09 16:30:16 +0530

    Scene to generate final output for rtnw added

--- Raytracing the Next Week/Raytracing/src/Renderer.cpp
+++ Raytracing the Next Week/Raytracing/src/Renderer.cpp
@@ -14,15 +14,16 @@
 #pragma region FUNCTION_SIGNATURES
 	HitableList GetWorld();
 	HitableList GetCornellBox();
+	HitableList GetfinalScene();
 	Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth, bool isUseSkybox=true);
 	void GetChunkRange(int& Start, int& End);
 	void RenderImage(int ThreadIndex, HitableList& World, Camera& cam);
 #pragma endregion
 	   
 #pragma region PUBLIC_VARIABLES
-	const int imageWidth = 500;
-	const int imageHeight = 500;
-	const int Samples = 2000;
+	const int imageWidth = 192;
+	const int imageHeight = 108;
+	const int Samples = 100;
 	const int MaxDepth = 60;
 
 	const int NumberOfThreads = std::thread::hardware_concurrency();
@@ -41,15 +42,15 @@ int main()
 	std::cout << "P3\n";
 	std::cout << imageWidth << " " << imageHeight << "\n255\n"; 
 
-	Vector3 LookFrom(278, 278, -880);
-	Vector3 LookAt(278, 278, 0);
+	Vector3 LookFrom(100,230, -1000);
+	Vector3 LookAt(100, 230, 0);
 	Vector3 ViewUp(0, 1, 0);
 	double focalDistance = (LookFrom - LookAt).length();
 	double aperture = 0.02;
 	Camera cam(35,(imageWidth/(double)imageHeight),aperture,focalDistance,LookFrom,LookAt,ViewUp,0,1);
 	
 	//HitableList World = GetWorld();
-	HitableList World = GetCornellBox();
+	HitableList World = GetfinalScene();
 
 	auto StartTime = std::chrono::steady_clock::now();
 
@@ -114,26 +115,50 @@ HitableList GetCornellBox()
 	auto red = make_shared<Lambertian>(make_shared<ConstantTexture>(Vector3(0.65, 0.05, 0.05)));
 	auto white = make_shared<Lambertian>(make_shared<ConstantTexture>(Vector3(0.73, 0.73, 0.73)));
 	auto green = make_shared<Lambertian>(make_shared<ConstantTexture>(Vector3(0.12, 0.45, 0.15)));
-	auto light = make_shared<DiffuseLight>(make_shared<ConstantTexture>(Vector3(7, 7, 7)));
-	auto isoLight = make_shared<Isotropic>(make_shared<PerlinTexture>(.02,8, PerlinTexture::NoiseType::PerlinNoise), Vector3(0.1, 0.1, 0.2));
+	auto light = make_shared<DiffuseLight>(make_shared<ConstantTexture>(Vector3(6, 6, 6)));
+	auto isoLight = make_shared<Isotropic>(make_shared<PerlinTexture>(.04,8, PerlinTexture::NoiseType::PerlinNoise), Vector3(0.5, 0.5, 0.5));
 	auto isoDark = make_shared<Isotropic>(make_shared<ConstantTexture>(Vector3(0.7, 0.7, 0.7)));
  
-	HitableList World(make_shared<RectYZ>(0, 555, 0, 555, 555, green, true));
-	World.Add(make_shared<RectYZ>(0, 555, 0, 555, 0, red));
-	World.Add(make_shared<RectXZ>(0, 555, 0, 555, 555, white,true));
-	World.Add(make_shared<RectXZ>(0, 555, 0, 555, 0, white));
-	World.Add(make_shared<RectXY>(0, 555, 0, 555, 555, white));
-	World.Add(make_shared<RectXZ>(113, 443, 127, 432, 554, light, true));
+	HitableList World(make_shared<RectYZ>(150, 350, 0, 200, 200, green, true));
+	World.Add(make_shared<RectYZ>(150, 350, 0, 200, 0, red));
+	World.Add(make_shared<RectXZ>(0, 200, 0, 200, 350, white,true));
+	World.Add(make_shared<RectXZ>(0, 200, 0, 200, 150, white));
+	World.Add(make_shared<RectXY>(0, 200, 150, 350, 200, white));
+	World.Add(make_shared<RectXZ>(41, 160, 46, 156, 349, light, true));
 
-	shared_ptr<Hitable> tallCubiod = make_shared<RotateY>(make_shared<Cuboid>(Vector3(0, 0, 0), Vector3(165, 330, 165),white), -15);
-	tallCubiod = make_shared<Translate>(tallCubiod, Vector3(130,0,295));
-	tallCubiod = make_shared<Volume>(isoLight, tallCubiod, 0.015,false);
-	shared_ptr<Hitable> smolCuboid = make_shared<RotateY>(make_shared<Cuboid>(Vector3(0, 0, 0), Vector3(165, 165, 165), white), 18);
-	smolCuboid = make_shared<Translate>(smolCuboid, Vector3(265, 0, 65));
+	shared_ptr<Hitable> tallCubiod = make_shared<RotateY>(make_shared<Cuboid>(Vector3(-400, -400, -400), Vector3(400, 400, 400),white), -15);
+	//tallCubiod = make_shared<Translate>(tallCubiod, Vector3(130,0,295));
+	tallCubiod = make_shared<Volume>(isoLight, tallCubiod, 0.005,false);
+	shared_ptr<Hitable> smolCuboid = make_shared<RotateY>(make_shared<Cuboid>(Vector3(0, 0, 0), Vector3(70, 70, 70), white), 18);
+	smolCuboid = make_shared<Translate>(smolCuboid, Vector3(100, 0, 65));
 	smolCuboid = make_shared<Volume>(isoDark, smolCuboid, 0.01);
+	shared_ptr<Hitable> Stand = make_shared<Cuboid>(Vector3(-20, -800, -20), Vector3(220, -0.5, 200), white);
 	World.Add(smolCuboid);
-	World.Add(tallCubiod);
-	return HitableList(make_shared<bvh_node>(World, 0, 1));
+	//World.Add(tallCubiod);
+	World.Add(Stand);
+	//return HitableList(make_shared<bvh_node>(World, 0, 1));
+	return World;
+}
+
+HitableList GetfinalScene()
+{
+	HitableList boxes;
+	double x, x1, z, z1,y;
+	auto white = make_shared<Lambertian>(make_shared<ConstantTexture>(Vector3(0.75, 0.75, 0.75)));
+	for(int i=0;i<20;i++)
+		for (int j = 0; j < 20; j++)
+		{
+			x = -400 + i * 50;
+			z = -400 + j * 50;
+			x1 = x + 50;
+			z1 = z + 50;
+			y = RandomDouble(10, 100);
+			boxes.Add(make_shared<Cuboid>(Vector3(x, 0, z), Vector3(x1, y, z1), white));
+		}
+	HitableList Cornell = GetCornellBox();
+	HitableList objs(make_shared<bvh_node>(boxes, 0, 1));
+	objs.Add(make_shared<bvh_node>(Cornell,0,1));
+	return objs;
 }
 
 Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth,  bool isUseSkybox)
@@ -159,8 +184,8 @@ Vector3 ColorAtRay(const Ray& ray, HitableList& world, int depth,  bool isUseSky
 			return BackgroundColor;
 		Vector3 NormalizedDirection = ray.Ray_Direction().normalized();
 		double NormalizedY = 0.5*(NormalizedDirection.y() + 1);		
-		NormalizedY = Clamp(Remap(NormalizedY, 0.4, 0.6, 0, 1),0,1);
-		Vector3 finalColor = (1 - NormalizedY)*Vector3(171, 169, 197) / 255 + NormalizedY * Vector3(225, 194, 183) / 255;
+		NormalizedY = Clamp(Remap(NormalizedY, 0.0, 0.6, 0, 1),0,1);
+		Vector3 finalColor = (1 - NormalizedY)*Vector3(225, 194, 183)*0.8 / 255 + NormalizedY * Vector3(171, 169, 197)*0.3 / 255;
 		return finalColor * finalColor;
 	}
 }
